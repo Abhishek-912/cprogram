18f64604638a34ad52fe034c23677426919be798
Cloud User <ec2-user@ip-172-31-16-179.us-east-2.compute.internal>
2021-04-06 06:41:09 +0000

    added

---
+++ bigg3.c
@@ -0,0 +1,17 @@
+  
+#include <stdio.h>
+void bigg3() {
+    int number;
+
+    printf("Give an integer: ");
+    scanf("%d", &number);
+
+    // true if number is less than 0
+    if (number < 0) {
+        printf("You entered %d.\n", number);
+    }
+
+    printf("The if statement is easy.");
+
+   // return 0;
+}
